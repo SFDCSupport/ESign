bf656237aa3c6a8d0c43406f685ec74fd95fdff7
Anand Pilania <Anand.Pilania@niit.com>
2024-01-12 11:55:56 +0530

    improved renderer

--- resources/views/partials/renderer.blade.php
+++ resources/views/partials/renderer.blade.php
@@ -1,9 +1,15 @@
 @pushonce('js')
     <script src="{{ url('vendor/esign/js/pdf.js') }}?legacy"></script>
+    <script src="{{ url('/vendor/esign/js/fabric.min.js') }}?4.6.0"></script>
     <script src="{{ url('vendor/esign/js/signature_pad.umd.min.js') }}?3.0.0-beta.3"></script>
     <script>
+        const pdfRenderTasks = [];
+        const pdfPages = [];
+        const canvasEditions = [];
+
         const loadPDF = (url, viewer) => {
             const pdfjsLib = window['pdfjs-dist/build/pdf'];
+
             pdfjsLib.GlobalWorkerOptions.workerSrc =
                 '/vendor/esign/js/pdf.worker.js?legacy';
 
@@ -12,21 +18,58 @@
 
                 for (let pageNum = 1; pageNum <= numPages; pageNum++) {
                     pdfDoc.getPage(pageNum).then((page) => {
-                        const viewport = page.getViewport({ scale: 1.5 });
-                        const canvas = document.createElement('canvas');
-                        const context = canvas.getContext('2d');
+                        let scale = 1.5;
+                        let viewport = page.getViewport({ scale: scale });
 
-                        canvas.height = viewport.height;
-                        canvas.width = viewport.width;
+                        if (viewport.width > viewer[0].clientWidth - 40) {
+                            viewport = page.getViewport({ scale: 1 });
+                            scale =
+                                (viewer[0].clientWidth - 40) / viewport.width;
+                            viewport = page.getViewport({ scale: scale });
+                        }
 
-                        viewer.append(canvas);
+                        let pageIndex = page.pageNumber - 1;
+
+                        viewer.append(`
+                            <div class="position-relative mt-1 ms-1 me-1 d-inline-block" id="canvas-container-${pageIndex}">
+                                <canvas id="canvas-pdf-${pageIndex}" class="shadow-sm canvas-pdf"></canvas>
+                                <div class="position-absolute top-0 start-0">
+                                    <canvas id="canvas-edition-${pageIndex}"></canvas>
+                                </div>
+                            </div>
+                        `);
+
+                        const canvasPdf = $(`#canvas-pdf-${pageIndex}`);
+                        const canvasEditionHTML = $(
+                            `#canvas-edition-${pageIndex}`,
+                        );
+                        const context = canvasPdf[0].getContext('2d');
+
+                        canvasPdf[0].height = viewport.height;
+                        canvasPdf[0].width = viewport.width;
+                        canvasEditionHTML[0].height = canvasPdf[0].height;
+                        canvasEditionHTML[0].width = canvasPdf[0].width;
 
                         const renderContext = {
                             canvasContext: context,
                             viewport: viewport,
+                            enhanceTextSelection: true,
                         };
 
-                        page.render(renderContext);
+                        const renderTask = page.render(renderContext);
+
+                        pdfRenderTasks.push(renderTask);
+                        pdfPages.push(page);
+
+                        const canvasEdition = new fabric.Canvas(
+                            'canvas-edition-' + pageIndex,
+                            {
+                                selection: false,
+                                allowTouchScrolling: true,
+                            },
+                        );
+
+                        canvasEditions.push(canvasEdition);
                     });
                 }
 
