f529dab6367df82d41f5fb3a58c6b474c0b27fbc
Anand Pilania <Anand.Pilania@niit.com>
2024-01-17 16:00:08 +0530

    WIP: canvas;

--- resources/views/documents/partials/party.blade.php
+++ resources/views/documents/partials/party.blade.php
@@ -2,17 +2,19 @@
     <a href="javascript: void(0)" class="partyLabel">
         {{ $signer->label ?? __('esign::label.nth_party', ['nth' => ordinal($loop->iteration ?? 1)]) }}
     </a>
-    <div class="flex items-center space-x-1 deleted-updown-ele align-items-center">
-     <a href="#" class="updown-docs-btn">
-        <i class="fa-solid fa-arrows-up-down"></i>    
-    </a>    
-    <a
-        data-party="{{ $loop->iteration ?? 1 }}"
-        href="javascript: void(0)"
-        class="deleted-party partyDelete"
-        onclick="partyRemove(this)"
+    <div
+        class="flex items-center space-x-1 deleted-updown-ele align-items-center"
     >
-        <i class="fa fa-trash"></i>
-    </a>
-</div>
+        <a href="#" class="updown-docs-btn">
+            <i class="fa-solid fa-arrows-up-down"></i>
+        </a>
+        <a
+            data-party="{{ $loop->iteration ?? 1 }}"
+            href="javascript: void(0)"
+            class="deleted-party partyDelete"
+            onclick="partyRemove(this)"
+        >
+            <i class="fa fa-trash"></i>
+        </a>
+    </div>
 </li>
