56e6cf7860761b629106cdbe79bbbbd7cf10de83
Rahul.1 <rahul.1.thakur@niit.com>
2024-01-17 14:45:00 +0530

    update party blade ui

--- resources/views/documents/partials/party.blade.php
+++ resources/views/documents/partials/party.blade.php
@@ -2,6 +2,10 @@
     <a href="javascript: void(0)" class="partyLabel">
         {{ $signer->label ?? __('esign::label.nth_party', ['nth' => ordinal($loop->iteration ?? 1)]) }}
     </a>
+    <div class="flex items-center space-x-1 deleted-updown-ele align-items-center">
+     <a href="#" class="updown-docs-btn">
+        <i class="fa-solid fa-arrows-up-down"></i>    
+    </a>    
     <a
         data-party="{{ $loop->iteration ?? 1 }}"
         href="javascript: void(0)"
@@ -10,4 +14,5 @@
     >
         <i class="fa fa-trash"></i>
     </a>
+</div>
 </li>
