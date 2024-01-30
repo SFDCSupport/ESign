58252069b6df408fe51999d652fb67f5177bc884
Rahul.1 <rahul.1.thakur@niit.com>
2024-01-30 10:46:05 +0530

    updated html for add signer modal

--- resources/assets/js/script.js
+++ resources/assets/js/script.js
@@ -1,3 +1,6 @@
 $(() => {
-
+    $(document).on('click', '[data-toggle="section"]', function(){
+        let target = $(this).data('target');
+        $(target).toggleClass('d-none');
+    })
 });
--- resources/views/documents/modals/send-mail-to-recipient.blade.php
+++ resources/views/documents/modals/send-mail-to-recipient.blade.php
@@ -29,8 +29,8 @@
                         name="documentId"
                         value="{{ $document->id }}"
                     />
-                    <div class="mb-3">
-                        <label for="formControlInput1" class="col-form-label">
+                    <div class="dark-bg-card p-2 py-3 rounded mb-3">
+                        <label for="formControlInput1" class="col-form-label pt-1 pb-1">
                             {{ __('esign::label.nth_signer', ['nth' => ordinal(1)]) }}
                         </label>
                         <input
@@ -39,7 +39,8 @@
                             placeholder="{{ __('esign::label.type_email_here') }}"
                         />
                     </div>
-                    <div>
+                    <div class="col-sm-12 mt-3 d-flex justify-content-between">
+                       <div class="col"> 
                         <div class="form-check mb-2">
                             <input
                                 class="form-check-input"
@@ -59,16 +60,42 @@
                                 class="form-check-input"
                                 type="checkbox"
                                 value=""
-                                id="flexCheckDefault"
+                                id="preserve_order"
                             />
                             <label
                                 class="form-check-label"
-                                for="flexCheckDefault"
+                                for="preserve_order"
                             >
                                 {{ __('esign::label.preserve_order') }}
                             </label>
                         </div>
                     </div>
+                        <div class="col text-end">
+                            <div class="form-check">
+                                <a class="editmessage-link" id="" href="#" aria-valuemax="" data-toggle="section" data-target="#mailSection">Edit Message</a>
+                              </div>
+                        </div>
+                    </div>
+
+
+                    <div class="col-sm-12">
+            
+                        <div class="dark-bg-card p-2 rounded mb-3 mt-3 d-none" id="mailSection">
+                          <div class="row">
+                            <div class="col-sm-12 mb-3">
+                              <label for="firstparty" class="form-label mb-1">Subject</label>
+                              <input class="form-control form-control" type="text" value="You are invited to submit a form" placeholder="Subject" aria-label="">
+                            </div>
+                  
+                            <div class="col-sm-12 mb-3">
+                              <label for="bodysubject" class="form-label mb-1">Body &nbsp;<i class="fa fa-info-circle"></i></label>
+                              <textarea class="form-control" id="bodysubject" rows="9&quot;" placeholder="Body"></textarea>
+                            </div>
+                  
+                          </div>
+                  
+                          </div>
+                      </div>
                 </form>
             </div>
             <div class="modal-footer">
