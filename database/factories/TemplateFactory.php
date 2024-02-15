03cc15885ad676694049901bf07b06540c1f3b9a
Anand Pilania <Anand.Pilania@niit.com>
2024-02-15 18:53:28 +0530

    WIP

--- database/factories/TemplateFactory.php
+++ database/factories/TemplateFactory.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Database\Factories;
+namespace NIIT\ESign\Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use NIIT\ESign\Models\Template;
--- database/seederes/ESignSeeder.php
+++ database/seederes/ESignSeeder.php
@@ -1,39 +1,32 @@
 <?php
 
-namespace Database\Seeders;
+namespace NIIT\ESign\Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Str;
 use NIIT\ESign\Models\Template;
 
 class ESignSeeder extends Seeder
 {
     public function run(): void
     {
-        foreach ([] as $template) {
-            Template::create($template);
+        Schema::disableForeignKeyConstraints();
+        Template::truncate();
+        Schema::enableForeignKeyConstraints();
+
+        foreach (glob(__DIR__.'/../../resources/views/mails/*.blade.php') as $file) {
+            $filename = Str::of(pathinfo($file, PATHINFO_FILENAME))
+                ->title()
+                ->replace('.blade', '')
+                ->replace('-', ' ')
+                ->value();
+
+            Template::create([
+                'title' => $filename,
+                'body' => file_get_contents($file),
+            ]);
         }
-    }
-
-    protected function signingLink(): array
-    {
-        return [
-
-        ];
-    }
-
-    protected function signingCompleted(): void
-    {
-        //
-    }
-
-    protected function signedByAll(): void
-    {
-        //
-    }
-
-    protected function signingPending(): void
-    {
-        //
     }
 }
